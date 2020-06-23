4af74955df1fb44e67a82ea88194c8c5765145a8
jobala <japhethobalak@gmail.com>
2020-06-24 01:10:32 +0300

    Formatting fixes

--- msgraph/cli/core/commands/client_factory.py
+++ msgraph/cli/core/commands/client_factory.py
@@ -6,9 +6,11 @@ from msgraphcore import GraphSession
 
 def resolve_client_arg_name(operation, kwargs):
     if not isinstance(operation, str):
-        raise CLIError("operation should be type 'str'. Got '{}'".format(type(operation)))
+        raise CLIError(
+            "operation should be type 'str'. Got '{}'".format(type(operation)))
     if 'client_arg_name' in kwargs:
-        logger.info("Keyword 'client_arg_name' is deprecated and should be removed.")
+        logger.info(
+            "Keyword 'client_arg_name' is deprecated and should be removed.")
         return kwargs['client_arg_name']
     path, op_path = operation.split('#', 1)
     path_comps = path.split('.')
@@ -29,17 +31,18 @@ def resolve_client_arg_name(operation, kwargs):
 
 
 def get_mgmt_service_client(cli_ctx,
-                             client_type,
-                             subscription_bound=True,
-                             subscription_id=None,
-                             api_version=None,
-                             base_url_bound=True,
-                             resource='https://graph.microsoft.com',
-                             sdk_profile=None,
-                             aux_subscriptions=None,
-                             aux_tenants=None,
-                             **kwargs):
-    browser_credential = InteractiveBrowserCredential(client_id='f7218512-c727-4138-9fb9-a0fe2500650c')
+                            client_type,
+                            subscription_bound=True,
+                            subscription_id=None,
+                            api_version=None,
+                            base_url_bound=True,
+                            resource='https://graph.microsoft.com',
+                            sdk_profile=None,
+                            aux_subscriptions=None,
+                            aux_tenants=None,
+                            **kwargs):
+    browser_credential = InteractiveBrowserCredential(
+        client_id='f7218512-c727-4138-9fb9-a0fe2500650c')
     graph_session = GraphSession(browser_credential)
     client = client_type({}, session=graph_session)
     return client
