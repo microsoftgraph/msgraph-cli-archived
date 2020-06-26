6a98c294e5b58cc5789f2b2874b3d46912fc0450
jobala <japhethobalak@gmail.com>
2020-06-26 09:58:39 +0300

    Invoke cli in __main__.py

---
+++ src/msgraph-cli/msgraph/__main__.py
@@ -0,0 +1,14 @@
+import sys
+
+from msgraph.cli.core import get_default_cli
+
+
+mg_cli = get_default_cli()
+
+
+def cli_main(cli, args):
+    return cli.invoke(args)
+
+
+exit_code = cli_main(mg_cli, sys.argv[1:])
+sys.exit(exit_code)
