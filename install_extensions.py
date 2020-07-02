a2eeae09dcbc49ef9accde47febf52e5a6fa564b
jobala <japhethobalak@gmail.com>
2020-07-02 11:27:25 +0300

    Programmatically install generated extensions

---
+++ install_extensions.py
@@ -0,0 +1,26 @@
+"""
+This script installs generated extensions and creates a manifest of installed extensions.
+"""
+import os
+import subprocess
+import sys
+from os import path
+from setuptools import sandbox
+
+path_to_extensions = path.join(os.getcwd(), 'msgraph-cli-extensions', 'src')
+
+
+def install_extension(extensions_directory=sys.argv[1]):
+    extensions = os.listdir(extensions_directory)
+    for extension in extensions:
+        sandbox.run_setup(path.join(path_to_extensions, extension,
+                                    'setup.py'), ['clean', 'bdist_wheel'])
+        subprocess.check_call(
+            [sys.executable, "-m", "pip", "install", path.join(path_to_extensions, extension, 'dist', '{}-0.1.0-py3-none-any.whl'.format(extension))])
+
+
+def add_to_manifest():
+    pass
+
+
+install_extension()
