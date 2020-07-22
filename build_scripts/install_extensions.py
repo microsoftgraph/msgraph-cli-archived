6954f1b59fb1d216bc2fbb2b3f725ee873b8f04d
jobala <japhethobalak@gmail.com>
2020-07-22 13:27:39 +0300

    Print error messages

--- build_scripts/install_extensions.py
+++ build_scripts/install_extensions.py
@@ -12,8 +12,10 @@ path_to_extensions = path.join(os.getcwd(), os.pardir, 'msgraph-cli-extensions',
 def install_extension(extensions_directory=path_to_extensions):
     extensions = os.listdir(extensions_directory)
     for extension in extensions:
-        subprocess.check_call(
-            [sys.executable, "-m", "pip", "install", path.join(path_to_extensions, extension, 'dist', '{}-0.1.0-py3-none-any.whl'.format(extension))])
-
+        try:
+            subprocess.check_call(
+                [sys.executable, "-m", "pip", "install", path.join(path_to_extensions, extension, 'dist', '{}-0.1.0-py3-none-any.whl'.format(extension))])
+        except Exception as error:
+            print(error)
 
 install_extension()
