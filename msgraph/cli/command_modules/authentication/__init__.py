515e00efa91cf840321ccb29fc082c18625bd1c9
jobala <japhethobalak@gmail.com>
2020-08-15 09:51:18 +0300

    Add documentation

--- msgraph/cli/command_modules/authentication/__init__.py
+++ msgraph/cli/command_modules/authentication/__init__.py
@@ -1,13 +1,21 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+
 from msgraph.cli.core import AzCommandsLoader
 from msgraph.cli.core.commands import CliCommandType
 import msgraph.cli.command_modules.authentication._help
 
 
 class AuthenticationCommandsLoader(AzCommandsLoader):
+    """Provides authentication commands to msgraph-cli
+    """
     def __init__(self, cli_ctx, **kwargs):
         super().__init__(cli_ctx, **kwargs)
 
     def load_command_table(self, args):
+        # specify the implementation module ie custom
         template = CliCommandType(operations_tmpl='msgraph.cli.command_modules.authentication.custom#{}')
 
         with self.command_group('', template) as g:
