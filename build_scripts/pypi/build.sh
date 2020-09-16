73385db6fe686d91df736b062f734613c56d2693
jobala <japhethobalak@gmail.com>
2020-09-16 20:26:11 +0300

    Add debian build_scripts

---
+++ build_scripts/pypi/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -ev
+
+echo "Starting at `pwd`"
+
+: "${BUILD_STAGINGDIRECTORY:?BUILD_STAGINGDIRECTORY environment variable not set}"
+: "${BUILD_SOURCESDIRECTORY:=`cd $(dirname $0); cd ../../; pwd`}"
+
+cd $BUILD_SOURCESDIRECTORY
+
+echo "Search setup files from `pwd`."
+python --version
+
+pip install -U pip setuptools wheel
+pip list
+
+python setup.py bdist_wheel -d $BUILD_STAGINGDIRECTORY
+python setup.py sdist  -d $BUILD_STAGINGDIRECTORY
