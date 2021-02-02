2be7c690e4a5c6f7265318f6c5e8772559f2ec09
jobala <japhethobalak@gmail.com>
2021-02-02 11:06:29 +0300

    Add file from azure-cli message

--- msgraph/cli/core/commands/parameters.py
+++ msgraph/cli/core/commands/parameters.py
@@ -1,6 +1,7 @@
 # --------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
+# This file is extracted from azure-cli
 # --------------------------------------------------------------------------------------------
 
 import argparse
