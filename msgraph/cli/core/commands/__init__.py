6d6b31a2428a9aaecea1ee805f50f06a6e34fbc7
jobala <japhethobalak@gmail.com>
2021-04-23 11:28:56 +0300

    Stub show_custom_command

--- msgraph/cli/core/commands/__init__.py
+++ msgraph/cli/core/commands/__init__.py
@@ -96,10 +96,7 @@ class GraphCommandGroup(CommandGroup):
         return self._command(name, method_name=method_name, custom_command=True, **kwargs)
 
     def custom_show_command(self, name, method_name=None, **kwargs):
-<<<<<<< HEAD
         #TODO: Replace this facade with the real implementation
-=======
->>>>>>> dev
         return self._command(name, method_name=method_name, custom_command=True, **kwargs)
 
     def _command(self, name, method_name, custom_command=False, **kwargs):
