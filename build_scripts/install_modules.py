6b374754824ddd031f1dda343f71d69b6459d06a
jobala <japhethobalak@gmail.com>
2021-06-23 16:12:50 +0300

    removes usersactions

--- build_scripts/install_modules.py
+++ build_scripts/install_modules.py
@@ -7,12 +7,14 @@ def install():
     command_modules = path.join(getcwd(), pardir, 'msgraph', 'cli', 'command_modules')
 
     for module in listdir(command_modules):
-        try:
-            subprocess.check_call(
-                [sys.executable, '-m', 'pip', 'install',
-                 path.join(command_modules, module, '.')])
-        except Exception as err:
-            print(err)
+        if path.isdir(path.join(command_modules, module)):
+            try:
+                subprocess.check_call([
+                    sys.executable, '-m', 'pip', 'install',
+                    path.join(command_modules, module, '.')
+                ])
+            except Exception as err:
+                print(err)
 
 
 if __name__ == '__main__':
