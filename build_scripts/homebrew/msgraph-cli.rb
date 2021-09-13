680386f21ee656fae6dec9d9e5148e60b0412d68
jobala <japhethobalak@gmail.com>
2021-09-13 11:31:37 +0300

    update formula template

--- build_scripts/homebrew/msgraph-cli.rb
+++ build_scripts/homebrew/msgraph-cli.rb
@@ -3,9 +3,9 @@ class MsgraphCli < Formula
 
   desc "Python based command line tools for interacting with Microsoft Graph"
   homepage "https://developer.microsoft.com/en-us/graph"
-  version "{{ cli_version }}"
-  url "{{ github_release_tar }}"
-  sha256 "{{ sha256 }}"
+  version "0.1.5.1"
+  url "https://codeload.github.com/microsoftgraph/msgraph-cli/tar.gz/refs/tags/0.1.5.1"
+  sha256 "9875f0f729bde45290f941cc9e3e80be8eb8a48ce51eb745465c86e4161c9e7d"
   license "MIT"
 
   depends_on "openssl@1.1"
@@ -17,7 +17,10 @@ class MsgraphCli < Formula
     depends_on "pkg-config" => :build
   end
 
-  {{resources}}
+    resource "msgraph" do
+    url "https://files.pythonhosted.org/packages/99/73/487a66f4cb58ef5b6246b67923850a79389f4fcceaf44526edf084eb2a7e/msgraph-0.1.2.tar.gz"
+    sha256 "11f46f930c14958499479101c528ece87744498e81310b163d1bfd6a9598b441"
+  end
 
   def install
     # Work around Xcode 11 clang bug
@@ -29,8 +32,8 @@ class MsgraphCli < Formula
 
     extensions = Dir.glob('msgraph/cli/command_modules/*')
 
-   # install msgraph module
-   venv.instance_variable_get(:@formula).system venv.instance_variable_get(:@venv_root)/"bin/pip", "install", buildpath
+    # install msgraph module
+    venv.instance_variable_get(:@formula).system venv.instance_variable_get(:@venv_root)/"bin/pip", "install", buildpath
 
    # Install extensions
     extensions.each do |extension|
@@ -46,11 +49,11 @@ class MsgraphCli < Formula
       end
     end
 
-    (bin/"mgc").write <<~EOS
+    (bin/"graph").write <<~EOS
       #!/usr/bin/env bash
       MG_INSTALLER=HOMEBREW #{libexec}/bin/python -m msgraph.cli \"$@\"
     EOS
 
-    bash_completion.install "az.completion" => "mgc"
+    bash_completion.install "az.completion" => "graph"
   end
 end
