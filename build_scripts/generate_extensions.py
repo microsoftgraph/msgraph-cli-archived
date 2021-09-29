d454a7b72d00aae4748cb6c65b7cae5bf7dc0d96
jobala <japhethobalak@gmail.com>
2021-09-29 12:11:08 +0300

    updates operation ids so as to have short command group names

--- build_scripts/generate_extensions.py
+++ build_scripts/generate_extensions.py
@@ -2,6 +2,7 @@ import os
 import sys
 from os import path
 import subprocess
+from yaml import FullLoader, load, dump
 
 
 def generate_extension_from_open_api_description(version):
@@ -11,20 +12,23 @@ def generate_extension_from_open_api_description(version):
         file_name, file_path = item
         file_name = remove_file_extension_and_group(file_name)
 
-        # Config files are used to modify generated extensions
-        generate_az_config_for(file_name, version)
-        generate_cli_config_for(file_name, version)
-        generate_python_config_for(file_name)
+        if file_name == 'applications':
+            update_operationId(file_path)
+            # Config files are used to modify generated extensions
+            generate_az_config_for(file_name, version)
+            generate_cli_config_for(file_name, version)
+            generate_python_config_for(file_name)
 
-        args = [
-            'autorest',
-            '--clear-output-folder',
-            '--az',
-            f'''--input-file={file_path}''',
-            f'''--azure-cli-extension-folder=../msgraph-cli-extensions/{version}''',
-        ]
+            args = [
+                'autorest',
+                '--version=3.3.2',
+                '--clear-output-folder',
+                '--az',
+                f'''--input-file={file_path}''',
+                f'''--azure-cli-extension-folder=../msgraph-cli-extensions/{version}''',
+            ]
 
-        subprocess.run(args, shell=True)
+            subprocess.run(args, shell=True)
 
 
 def get_open_api_descriptions(version: str):
@@ -38,6 +42,27 @@ def get_open_api_descriptions(version: str):
         result.append(file_and_path)
 
     return result
+
+
+def update_operationId(file: str):
+    data = None
+
+    with open(file, 'r') as f:
+        data = load(f.read(), Loader=FullLoader)
+        for path in data['paths']:
+            verbs = data['paths'][path].keys()
+
+            for verb in verbs:
+                op_id = data['paths'][path][verb]['operationId']
+                command_group, action = op_id.split('_')
+
+                if '.' in command_group:
+                    command_group = command_group.split('.')[1]
+
+                data['paths'][path][verb]['operationId'] = '{}_{}'.format(command_group, action)
+
+    with open(file, 'w+') as f:
+        f.write(dump(data))
 
 
 def remove_file_extension_and_group(file_name):
@@ -120,73 +145,41 @@ directive:
       set:
           group: {file_name}
     - where:
-          group: {parsed_file_name}-{parsed_file_name}
-      set:
-          group: {parsed_file_name}
-    - where:
-          command: {file_name} {parsed_file_name} create-{parsed_file_name}
-      set:
-          command: {file_name} {parsed_file_name} create
-    - where:
-          command: {file_name} {parsed_file_name} get-{parsed_file_name}
-      set:
-          command: {file_name} {parsed_file_name} get
-    - where:
-          command: {file_name} {parsed_file_name} list-{parsed_file_name}
-      set:
-          command: {file_name} {parsed_file_name} list
-    - where:
-          command: {file_name} {parsed_file_name} update-{parsed_file_name}
-      set:
-          command: {file_name} {parsed_file_name} update
-    - where:
-          command: {file_name} {parsed_file_name} add-{parsed_file_name}
-      set:
-          command: {file_name} {parsed_file_name} add
-    - where:
-          command: {file_name} {parsed_file_name} set-{parsed_file_name}
-      set:
-          command: {file_name} {parsed_file_name} set
-    - where:
-          command: {file_name} {parsed_file_name} show-{parsed_file_name}
-      set:
-          command: {file_name} {parsed_file_name} show
-    - where:
-          command: {file_name} {parsed_file_name} delete-{parsed_file_name}
-      set:
-          command: {file_name} {parsed_file_name} delete
-    - where:
           command: (.*)(create-)(.*)
       set:
-          command: $1 $3 create
+          command: $1$3 create
     - where:
           command: (.*)(get-)(.*)
       set:
-          command: $1 $3 get
+          command: $1$3 get
     - where:
           command: (.*)(list-)(.*)
       set:
-          command: $1 $3 list
+          command: $1$3 list
     - where:
           command: (.*)(update-)(.*)
       set:
-          command: $1 $3 update
-    - where:
-          command: (.*)(add-)(.*)
-      set:
-          command: $1 $3 add
-    - where:
-          command: (.*)(set-)(.*)
-      set:
-          command: $1 $3 set
+          command: $1$3 update
     - where:
           command: (.*)(show-)(.*)
       set:
-          command: $1 $3 show
+          command: $1$3 show
     - where:
           command: (.*)(delete-)(.*)
       set:
-          command: $1 $3 delete
+          command: $1$3 delete
+    - where:
+          command: (.*)(add-)(.*)
+      set:
+          command: $1$3 add
+    - where:
+          command: (.*)(remove-)(.*)
+      set:
+          command: $1$3 remove
+    - where:
+          command: (.*)(set-)(.*)
+      set:
+          command: $1$3 set
 
 
 modelerfour:
